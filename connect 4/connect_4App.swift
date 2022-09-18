6dc817cad843090398d6ae7b30e1ac4965451fad
Najla <Najla.Alshehri8@gmail.com>
2022-09-18 09:50:46 +0300

    Initial Commit

---
+++ connect 4/connect_4App.swift
@@ -0,0 +1,17 @@
+//
+//  connect_4App.swift
+//  connect 4
+//
+//  Created by Najla on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct connect_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
